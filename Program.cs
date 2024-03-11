7b28fd49ffe312b51ec8e9e084bd264cb78412e5
alejocp00 <alex00cu@gmail.com>
2024-03-11 19:11:41 -0400

    Add Program.cs file with initial code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tester;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var generator = new ProblemGenerator(123);
+        var array = generator.GenerateArray(5, 10, 10);
+        var cArray = generator.GetCharArray();
+        foreach (var s in array)
+        {
+            Console.WriteLine(s);
+        }
+        foreach (var c in cArray)
+        {
+            Console.Write(c);
+        }
+    }
+}
