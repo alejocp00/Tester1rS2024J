56317eed56cf09f771b746c9f601495e30ae461c
alejocp00 <alex00cu@gmail.com>
2024-03-12 12:24:21 -0400

    Fix formatting in ProblemGenerator.cs

--- ProblemGenerator.cs
+++ ProblemGenerator.cs
@@ -5,8 +5,8 @@ namespace Tester;
 public class ProblemGenerator
 {
     public int Seed { get; }
-    private Random randomCharSelector { get;}
-    private  Random randomWordSizeSelector {get;}
+    private Random randomCharSelector { get; }
+    private Random randomWordSizeSelector { get; }
 
     private string[]? generatedArray;
     private char[]? generatedCharArray;
@@ -15,13 +15,13 @@ public class ProblemGenerator
     {
         Seed = seed;
         randomCharSelector = new Random(seed);
-        randomWordSizeSelector = new Random(seed+1);
-        
+        randomWordSizeSelector = new Random(seed + 1);
+
     }
 
     private string GenerateString(int minSize, int maxSize)
     {
-        char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 
         var size = randomWordSizeSelector.Next(minSize, maxSize);
@@ -34,7 +34,7 @@ public class ProblemGenerator
         return result.ToString();
     }
 
-    public string[] GenerateArray(int minStringSize, int maxStringSize, int arraySize)
+    private string[] GenerateArray(int minStringSize, int maxStringSize, int arraySize)
     {
         var result = new string[arraySize];
         for (int i = 0; i < arraySize; i++)
@@ -45,22 +45,22 @@ public class ProblemGenerator
         return result;
     }
 
-    public char[]? GetCharArray(int aSize = -1)
+    private char[]? GetCharArray(int aSize = -1)
     {
         if (generatedArray == null)
         {
             throw new InvalidOperationException("Array not generated");
         }
-        
-        int size = aSize == -1 ? randomCharSelector.Next(0, generatedArray.Length-1) : aSize;
+
+        int size = aSize == -1 ? randomCharSelector.Next(0, generatedArray.Length - 1) : aSize;
         char[] result = new char[size];
 
         List<Tuple<int, int>> indexes = new List<Tuple<int, int>>();
 
         for (int i = 0; i < result.Length; i++)
         {
-            var wordIndex = randomCharSelector.Next(0, generatedArray.Length-1);
-            var charIndex = randomCharSelector.Next(0, generatedArray[wordIndex].Length-1);
+            var wordIndex = randomCharSelector.Next(0, generatedArray.Length - 1);
+            var charIndex = randomCharSelector.Next(0, generatedArray[wordIndex].Length - 1);
             indexes.Add(new Tuple<int, int>(wordIndex, charIndex));
         }
 
@@ -72,4 +72,5 @@ public class ProblemGenerator
         generatedCharArray = result.Clone() as char[];
         return result;
     }
+    public Tuple<string[], char[]> GetProblem(int minStringSize, int maxStringSize, int sArraySize, int cArraySize = -1) => new Tuple<string[], char[]>(GenerateArray(minStringSize, maxStringSize, sArraySize), GetCharArray(cArraySize)!);
 }
