697686a5fc0d89747e816da5657a91dfce708ebd
alejocp00 <alex00cu@gmail.com>
2024-03-14 23:58:41 -0400

    Add System.Text.Json namespace and refactor char array initialization

--- ProblemGenerator.cs
+++ ProblemGenerator.cs
@@ -1,5 +1,6 @@
 
 using System.Text;
+using System.Text.Json;
 
 namespace Tester;
 public class ProblemGenerator
@@ -7,9 +8,7 @@ public class ProblemGenerator
     public int Seed { get; }
     private Random randomCharSelector { get; }
     private Random randomWordSizeSelector { get; }
-
-    private string[]? generatedArray;
-    private char[]? generatedCharArray;
+    private char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
     public ProblemGenerator(int seed)
     {
@@ -21,9 +20,6 @@ public class ProblemGenerator
 
     private string GenerateString(int minSize, int maxSize)
     {
-        char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
-
         var size = randomWordSizeSelector.Next(minSize, maxSize);
         var result = new StringBuilder(size);
         for (int i = 0; i < size; i++)
@@ -41,36 +37,61 @@ public class ProblemGenerator
         {
             result[i] = GenerateString(minStringSize, maxStringSize);
         }
-        generatedArray = result.Clone() as string[];
         return result;
     }
 
-    private char[]? GetCharArray(int aSize = -1)
+    private char[] GetCharArray(int aSize = -1)
     {
-        if (generatedArray == null)
-        {
-            throw new InvalidOperationException("Array not generated");
-        }
-
-        int size = aSize == -1 ? randomCharSelector.Next(0, generatedArray.Length - 1) : aSize;
+        int maxArraySize = 100;
+        int size = aSize == -1 ? randomCharSelector.Next(0, maxArraySize) : aSize;
         char[] result = new char[size];
 
-        List<Tuple<int, int>> indexes = new List<Tuple<int, int>>();
-
-        for (int i = 0; i < result.Length; i++)
+        for (int i = 0; i < size; i++)
         {
-            var wordIndex = randomCharSelector.Next(0, generatedArray.Length - 1);
-            var charIndex = randomCharSelector.Next(0, generatedArray[wordIndex].Length - 1);
-            indexes.Add(new Tuple<int, int>(wordIndex, charIndex));
+            result[i] = chars[randomCharSelector.Next(chars.Length)];
         }
-
-        for (int i = 0; i < result.Length; i++)
-        {
-            result[i] = generatedArray[indexes[i].Item1][indexes[i].Item2];
-        }
-
-        generatedCharArray = result.Clone() as char[];
         return result;
+
     }
     public Tuple<string[], char[]> GetProblem(int minStringSize, int maxStringSize, int sArraySize, int cArraySize = -1) => new Tuple<string[], char[]>(GenerateArray(minStringSize, maxStringSize, sArraySize), GetCharArray(cArraySize)!);
+}
+
+public class ProblemGestor
+{
+    public int Seed { get; }
+    public int SeedForLimits { get; }
+    private List<Tuple<string[], char[]>> problems { get; }
+    public ProblemGestor(int seed)
+    {
+        Seed = seed;
+        SeedForLimits = seed - 1;
+        problems = new List<Tuple<string[], char[]>>();
+    }
+
+    public List<Tuple<string[], char[]>> GetProblems(int amount, int minWordSize, int maxWordSize, int arraySize, int cArraySize)
+    {
+
+        if (problems.Count > 0)
+        {
+            return problems;
+        }
+
+        var random = new Random(SeedForLimits);
+        for (int i = 0; i < amount; i++)
+        {
+            var generator = new ProblemGenerator(Seed + i);
+            problems.Add(generator.GetProblem(minWordSize, maxWordSize, arraySize, cArraySize));
+        }
+        return problems;
+    }
+
+    public void ExportProblems(string path)
+    {
+        Dictionary<string, object> data = new Dictionary<string, object>();
+        data.Add("seed", Seed);
+        data.Add("seedForLimits", SeedForLimits);
+        data.Add("problems", problems);
+        var json = JsonSerializer.Serialize(data);
+        File.WriteAllText(path, json);
+    }
 }
